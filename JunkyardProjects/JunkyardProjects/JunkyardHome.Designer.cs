0593c225059d684794dd0d086eb8e0bc916def5a
VirtualFrost <virtuallfrost@gmail.com>
2016-10-05 14:01:55 +1000

    Removed Unfinished Program

--- JunkyardProjects/JunkyardProjects/JunkyardHome.Designer.cs
+++ JunkyardProjects/JunkyardProjects/JunkyardHome.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -43,19 +42,9 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(12, 41);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(120, 25);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Fibonacci Sequence";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(12, 72);
+            this.button3.Location = new System.Drawing.Point(12, 43);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(120, 23);
             this.button3.TabIndex = 2;
@@ -69,7 +58,6 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(503, 286);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "junkyardForm";
             this.Text = "Junkyard Projects";
@@ -80,7 +68,6 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
     }
 }
--- JunkyardProjects/JunkyardProjects/JunkyardHome.cs
+++ JunkyardProjects/JunkyardProjects/JunkyardHome.cs
@@ -23,12 +23,6 @@ namespace JunkyardProjects
             fizzbuzzForm.Show();
         }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            FibonacciSequence fibonacciSequenceForm = new FibonacciSequence(this);
-            fibonacciSequenceForm.Show();
-        }
-
         private void button3_Click(object sender, EventArgs e)
         {
             ToLeetSpeak toLeetSpeakForm = new ToLeetSpeak(this);
